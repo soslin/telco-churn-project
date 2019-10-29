8cac4769db74a1229a26c78844a9796ce73be742
soslin <seanoslin@gmail.com>
2019-10-28 21:58:29 -0500

    Next commit

--- wrangle_telco.py
+++ wrangle_telco.py
@@ -13,22 +13,18 @@ JOIN internet_service_types AS i
 	ON i.internet_service_type_id = c.internet_service_type_id
 JOIN payment_types AS p
 	ON c.payment_type_id = p.payment_type_id
-WHERE tenure > 0 OR total_charges > 0;''', url)
+WHERE (tenure > 0 OR total_charges > 0) AND tenure > 1;''', url)
     return telco_data
 wrangle_telco()
 
 telco_df = wrangle_telco()
 
 def peekatdata(telco_df):
-    telco_df.head()
-    telco_df.tail()
-    telco_df.sample(10)
-    telco_df.shape
-    telco_df.describe()
-    telco_df.info()
-    return telco_df
+    print(telco_df.head())
+    print(telco_df.tail())
+    print(telco_df.sample(10))
+    print(telco_df.shape)
+    print(telco_df.describe())
+    print(telco_df.info())
 peekatdata(telco_df)
 
-
-def df_value_counts(telco_df):
-    df.groupby('a').count()
